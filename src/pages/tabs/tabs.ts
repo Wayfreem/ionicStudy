71cfa0edd61baa2de52c00097c3e16c15d0ba56d
Wayfreem <wayfreem@163.com>
2018-03-24 16:17:43 +0800

    增加点击手机返回按钮时的事件

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
-import {NavParams, ViewController} from "ionic-angular";
+import {NavParams, Platform, Tabs, ViewController} from "ionic-angular";
+import {BackButtonService} from "../../services/backButton.service";
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+
+  @ViewChild('myTabs') tabRef: Tabs;
 
   tab1Root = HomePage;
   tab2Root = AboutPage;
@@ -15,7 +18,13 @@ export class TabsPage {
   tab4Root = 'SettingPage';   // 使用懒加载
 
   constructor(public navParams: NavParams,
-              public viewCtrl: ViewController) {
+              public viewCtrl: ViewController,
+              public platform: Platform,
+              public backButtonService: BackButtonService) {
+
+    platform.ready().then(() => {
+      this.backButtonService.registerBackButtonAction(this.tabRef);
+    });
   }
 
   ionViewDidLoad() {
