9ce075ae42fd2c39cfd86c8459be41e1ffa0e535
Wayfreem <wayfreem@163.com>
2018-03-29 18:04:10 +0800

    开始做录入单据的界面

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  dept: any;
 
+  dateTime = {
+    startTime: "",
+    endTime: ""
+  };
+
+  deptList = [
+    {value: 1, label:"ERP项目组"},
+    {value: 2, label:"IT运维组"},
+    {value: 3, label:"APP研发组"},
+    {value: 4, label:"HR组"},
+  ];
+
+  constructor(public navCtrl: NavController, public alerCtrl: AlertController) {
+
+  }
+
+  _submit(){
+    let alert = this.alerCtrl.create({
+      title: '提示消息',
+      message: '提交成功!',
+      buttons: ['确认']
+    });
+    alert.present()
   }
 
 }
