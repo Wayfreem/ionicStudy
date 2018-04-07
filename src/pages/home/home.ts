43eae4bf54e0c60f23e7ff5098bf1abe112c6ca1
Wayfreem <wayfreem@163.com>
2018-04-07 18:05:41 +0800

    删除没有用到的引入

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import {ApprovalPage} from "../approval/approval";
 
 @Component({
   selector: 'page-home',
