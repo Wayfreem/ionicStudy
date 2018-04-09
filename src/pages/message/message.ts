999e7b617e9682e2bde5c11d612d0e531ea54123
Wayfreem <wayfreem@163.com>
2018-04-09 19:26:32 +0800

    修改消息界面

--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -8,11 +8,60 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MessagePage {
 
+  messageList = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MessagePage');
+    this.messageList = this.getMessage();
+  }
+
+  getMessage() {
+    return [
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"My Neighbor Totoro",
+        description: "Hayao Miyazaki • 1988",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"Raiders of the Lost Ark",
+        description: "Steven Spielberg • 1981",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"Ghostbusters",
+        description: "Ivan Reitman • 1984",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"Batman",
+        description: "Tim Burton • 1988",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"Back to the Future",
+        description: "Robert Zemeckis • 1985",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"The Empire Strikes Back",
+        description: "Irvin Kershner • 1980",
+        buttonText: "查看"
+      },
+      {
+        src: "assets/img/contact/thumbnail-totoro.png",
+        label:"The Terminator",
+        description: "James Cameron • 1984",
+        buttonText: "查看"
+      },
+    ];
   }
 
 }
