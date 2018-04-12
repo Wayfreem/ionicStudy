8a7297f191f7404d4f402e13d1619b9332e6dc79
Wayfreem <wayfreem@163.com>
2018-04-12 13:44:30 +0800

    增加http调用的方法

---
+++ src/providers/http-service/http-client-service.ts
@@ -0,0 +1,121 @@
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest, HttpResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {ConfigProvider} from "../config/config";
+import {Observable} from "rxjs/Observable";
+import {Storage} from "@ionic/storage";
+import * as CryptoJS from 'crypto-js';
+import 'rxjs/Rx';
+import 'rxjs/add/observable/throw';
+
+export enum RequestMethod {
+  GET = 'GET',
+  HEAD = 'HEAD',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  OPTIONS = 'OPTIONS',
+  PATCH = 'PATCH'
+}
+
+@Injectable()
+export class HttpClientServiceProvider {
+
+  headers = new HttpHeaders({
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  });
+
+  constructor(public http: HttpClient,
+              public storage: Storage,
+              public config: ConfigProvider) {
+    console.log('Hello HttpServiceProvider Provider');
+  }
+
+  // 将数据转换为 httpParams
+  private serialize(obj: any): HttpParams {
+    let params = new HttpParams();
+
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        params = params.set(key, obj[key]);
+      }
+    }
+    return params;
+  }
+
+  private sign(param: object, secret: string) {
+    // 对参数名进行字典排序
+    const arrayParam: any[] = [];
+    for (const key in param) {
+      arrayParam.push(key);
+    }
+    arrayParam.sort();
+
+    // 拼接有序的参数名-值串
+    const paramArray = new Array();
+    paramArray.push(secret);
+
+    for (let i of arrayParam) {
+      paramArray.push(i + param[i]);
+    }
+    paramArray.push(secret);
+    // SHA-1编码，并转换成大写，即可获得签名
+    const shaSource = paramArray.join('');
+    const sign = CryptoJS.SHA1(shaSource).toString(CryptoJS.enc.Hex).toUpperCase();
+    return sign;
+  }
+
+  get(path: string, args?: any): Observable<any> {
+    const options = {
+      headers: this.headers,
+      withCredentials: true
+    };
+
+    if (args) {
+      options['params'] = this.serialize(args);
+    }
+
+    return this.http.get(path, options)
+      .catch(this.checkError.bind(this));
+  }
+
+  post(method: string, body: any, customHeaders?: HttpHeaders): Observable<any> {
+    const data = body || {};
+    data.method = method;
+    data.appKey = '00001';
+    const secret = 'abcdeabcdeabcdeabcdeabcde';
+    data.version = '1.0';
+    if (method.indexOf("login") == -1) {
+      data.sessionId = this.storage.get("USER_INFO");
+    }
+    data.sign = this.sign(data, secret);
+    return this.request(this.config.getPublicProductUrl(), this.serialize(data), RequestMethod.POST, customHeaders);
+  }
+
+  private request(path: string, body: any, method = RequestMethod.POST,  customerHeaders?: HttpHeaders): Observable<any> {
+    const req = new HttpRequest(method, path, body, {
+      headers: customerHeaders || this.headers,
+      withCredentials: true
+    });
+
+    return this.http.request(req)
+      .filter(response => response instanceof HttpResponse)
+      .map((response: HttpResponse<any>) => response.body)
+      .catch(error => this.checkError(error));
+  }
+
+
+  // 错误检查
+  private checkError(error: any): any {
+    if (error && error.status === 401) {
+      console.log('401---------------------');
+    } else {
+      console.log(error);
+    }
+    throw error;
+  }
+
+
+
+
+}
--- src/providers/http-service/http-service.ts
+++ src/providers/http-service/http-service.ts
@@ -1,11 +1,77 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Injectable} from "@angular/core";
+import {Headers, Http, Request, RequestMethod, RequestOptions, URLSearchParams} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+
+//55
+// export const BASE_URL = 'http://10.2.0.55:8080/yyerp/';
+
+// 外网
+export const BASE_URL = `http://14.23.148.134:8083/yyerp/`;
+
+//32
+// export const BASE_URL = 'http://10.2.0.32:8080/yyerp/';
+
+//36
+// export const BASE_URL = `http://10.2.0.36:8888/yyerp/`
+
+//本地
+//export const BASE_URL = `http://127.0.0.1:8081/yyerp/`
 
 @Injectable()
 export class HttpServiceProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello HttpServiceProvider Provider');
+  headers: Headers = new Headers({'content-type': 'application/x-www-form-urlencoded'});
+
+  constructor(private http: Http,) {
+  }
+
+  private serialize(obj: any): URLSearchParams {
+    let params = new URLSearchParams();
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        params.set(key, obj[key]);
+      }
+    }
+    return params;
+  }
+
+  public $get(path: string, params: URLSearchParams, contentType?: string) {
+    let url = `${BASE_URL}${path}`;
+    let requestOptions = this._createRequestOptions(url, RequestMethod.Get, params,);
+    return this.http.request(new Request(requestOptions))
+      .map(res => res.json())
+      .catch(
+        err => {
+          return Observable.throw(err);
+        })
+  }
+
+  public $post(path: string, params: object) {
+    let url = `${BASE_URL}${path}`;
+    let requestOptions = this._createRequestOptions(url, RequestMethod.Post, this.serialize(params));
+    return this.http.request(new Request(requestOptions)).map(res => res.json());
+  };
+
+
+  /**
+   * 生成请求项参数
+   * @param {string} $url 调用的地址
+   * @param {RequestMethod} $method 调用的方式（POST、GET 等）
+   * @param {URLSearchParams} $params 请求的参数
+   * @param $body 请求的body 可以为空
+   * @returns {RequestOptions}
+   * @private
+   */
+  private _createRequestOptions($url: string, $method: RequestMethod, $params: URLSearchParams = null, $body: any = null): RequestOptions {
+    let requestOptions: RequestOptions = new RequestOptions({
+      headers: this.headers,
+      method: $method,
+      url: $url,
+      params: $params,
+      body: $body
+    });
+
+    return requestOptions;
   }
 
 }
