90542f1d9c795420dc6d60faeaf7d0764ec71e87
Wayfreem <wayfreem@163.com>
2018-05-10 15:43:48 +0800

    修改配置文件

--- src/providers/config/config.ts
+++ src/providers/config/config.ts
@@ -12,12 +12,7 @@ export class ConfigProvider {
 
   getProductionUrl() {
     let urlConfig = {
-      localUrl: `http://127.0.0.1:8081/yyerp/`,
-      testUrl: `http://10.2.0.55:8080/yyerp/`,
-      _32Url: `http://10.2.0.32:8080/yyerp/`,
-      _36Url: `http://10.2.0.36:8888/yyerp/`,
-      productionUrl: `http://14.23.148.134:8083/yyerp/`,
-      newTestUrl: `http://10.2.4.196:6060/router/`,
+
       proxyUrl: `/yyerp/`,
       proxyUrl_new:`/yyerpNew/`
     };
