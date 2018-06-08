19991657844297fa8b8891e096aa111f2e871141
Wayfreem <wayfreem@163.com>
2018-06-08 09:48:57 +0800

    去掉调用 http的服务，不推荐使用

--- src/pages/approval/approval-home/approval-home.ts
+++ src/pages/approval/approval-home/approval-home.ts
@@ -2,7 +2,6 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams, Platform} from 'ionic-angular';
 import * as CryptoJS from 'crypto-js';
 
-import {HttpServiceProvider} from "../../../providers/http-service/http-service";
 import {HttpClientServiceProvider} from "../../../providers/http-service/http-client-service";
 
 @IonicPage()
@@ -17,15 +16,12 @@ export class ApprovalHomePage {
   items = [];
 
   httpClientData: any;
-  httpData: any;
   newERPData: any;
 
   constructor(public platform: Platform,
               public navCtrl: NavController,
               public navParams: NavParams,
-              public httpService: HttpServiceProvider,
               public httpClientService: HttpClientServiceProvider,) {
-
   }
 
   ionViewDidLoad() {
@@ -34,18 +30,6 @@ export class ApprovalHomePage {
 
   gotoListPage(event, item) {
     console.log(item);
-  }
-
-  _login() {
-    let url = 'app/loginAction/doNotNeedSession_login.action';
-    let pwd = CryptoJS.MD5("1230").toString(CryptoJS.enc.Hex);
-    let params = {CZYID: "0001131", CZYMM: pwd};
-    let data = {pdata: JSON.stringify(params)};
-    this.httpService.$post(url, data).subscribe(
-      (resultData) => {
-        this.httpData = resultData;
-        console.log(resultData);
-      });
   }
 
   getHttpData() {
